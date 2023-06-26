67075318d982b6b7de31dd2dad1b3be66e3f1af1
Carlos1507 <ayala.carlos@pucp.edu.pe>
2023-06-26 11:14:30 -0500

    inicio

---
+++ app/src/main/java/com/example/lab6_20190740_20195527/activities/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.lab6_20190740_20195527.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.lab6_20190740_20195527.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
