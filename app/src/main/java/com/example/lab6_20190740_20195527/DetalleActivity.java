a754aebd5bca7706da3feebbcdc2546b15450f73
niurkaa <niurka_30@outlook.es>
2023-06-26 17:46:58 -0500

    :)

---
+++ app/src/main/java/com/example/lab6_20190740_20195527/DetalleActivity.java
@@ -0,0 +1,14 @@
+package com.example.lab6_20190740_20195527;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetalleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalle);
+    }
+}
